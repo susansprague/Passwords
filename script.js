f7dcb1cb2a7de167f2b2d952825ba4b4be4e7f73
Your Name <you@example.com>
2023-04-20 12:41:02 -0400

    created javascript code to generate passwords correctly

--- script.js
+++ script.js
@@ -1,6 +1,43 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+// Generate password
+function generatePassword() {
+  let password = '';
+  let chars = "";
+  let passwordLength = parseInt(window.prompt("Please enter your password length"));
+  console.log(passwordLength)
+if(passwordLength<8 || passwordLength>128 || isNaN(passwordLength)) {
+  alert("Please enter a length between 8 and 128");
+  return "Please try again"
+} 
+if(confirm ("Would you like lowercase letters?")) {
+  chars += "abcdefghijklmnopqrstuvwxyz";
+}
+  if (confirm("Would you like uppercase letters?")) {
+    chars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+  if (confirm("Would you like numbers?")) {
+    chars += "0123456789";
+  }
+  if (confirm("Would you like specials?")) {
+    chars += "!@#$%&*()";
+  } 
+  console.log(chars)
+  if (chars==="") {
+    alert("Must choose at least one character type")
+    return "Please try again"
+  }
+  for(var i=0; i<passwordLength; i++) {
+    let charindex=Math.floor(Math.random()*chars.length)
+    let digit=chars.charAt(charindex)
+    password+=digit
+  }
+return password
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
